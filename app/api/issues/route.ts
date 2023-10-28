ee114a202c71d01ae9862a7b63666dd0e6ebba71
Bader Mohammad <bader97alyami@hotmail.com>
2023-10-29 01:27:28 +0300

    biuld API to cretated issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from 'zod';
+import prisma from '@/prisma/client'
+
+
+
+
+
+// validate the body request by **ZOD**
+const issuesZodSchema = z.object({
+    title: z.string().min(3).max(255),
+    description: z.string().min(5)
+})
+
+// validate the body request by **ZOD**
+
+export async function POST(request: NextRequest, res: Response) {
+    const body = await request.json();
+    const validtion = issuesZodSchema.safeParse(body);
+
+    if (!validtion.success) {
+        // res.json(validtion.error.errors, { status: 400 });
+        return NextResponse.json(validtion.error.errors, { status: 400 });
+    }
+
+    // const { title, description } = body;
+    const newIssues = await prisma.issue.create({
+        data: {
+            title: body.title,
+            description: body.description
+        }
+    })
+
+    return NextResponse.json(newIssues, { status: 201 });
+
+}
